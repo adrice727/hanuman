857a0c92d6c6b2daf3d990117eada6904a0556e6
Aaron Rice <adrice727@gmail.com>
2016-02-07 20:12:12 -0800

    Add forEach, map, and _error functions.

--- hanuman.js
+++ hanuman.js
@@ -1,6 +1,8 @@
 
-var k = (() => {
+var hanuman = (() => {
 
+	/** Accepts a message, and returns a new error */
+	let _error = (message) => new Error(`ø( ^_^ )ø Hanuman: ${message}`);
 
 	/**
 	* Returns a curried version of the function, which can also be curried
@@ -8,38 +10,71 @@ var k = (() => {
 	* @param {...*} [args] - A single argument or series of arguments
 	*/
 	let curry = (fn, args) => {
-	  
+
 	  args = args || [];
-	  
+
 	  return function() {
-	    
+
 	    let arity = fn.length;
 	    let combinedArgs = args.concat(Array.from(arguments));
-	    
-	    
+
 	    if ( combinedArgs.length === arity ) {
 	      return fn.apply(this, combinedArgs)
 	    } else {
 	      return curry(fn, combinedArgs);
 	    }
-	    
+
 	  }
-	  
-	}
-
-	let forEach = (fn, collection) => {
-
-
 
 	}
+
+  /** forEach Array */
+	let _forEachArray = (fn, array) => {
+
+		for ( let i = 0; i < array.length; i++ ) {
+			fn(array[i]);
+		}
+
+	};
+
+	/** forEach Object */
+	let _forEachObject = (fn, obj) => {
+
+		let keys = Object.keys(obj);
+		for ( let i = 0; i < keys.length; i++ ) {
+			fn(obj[keys[i]], keys[i], obj);
+		}
+
+	};
 
 	/**
-	* Creates a new collection by applying a function to each value in the collection
+	* Applies a function to each item in the collection.  If the collection is an object
+	* the iterator function will receive the key, value, and object.
+	* @param {function} fn
+	* @param {array | object} [args] - A single argument or series of arguments
+	*/
+	let forEach = (fn, collection) => {
+
+		if ( !Array.isArray(collection) || ( typeof collection !== 'object') ) { throw _error('Input must be an array or an object'); }
+
+		Array.isArray(collection) ? _forEachArray(fn, collection) : _forEachObject(fn, collection);
+
+	};
+
+	/**
+	* Creates a new list by applying a function to each item in the list
 	* @param {function} fn - The function to be called on each element
 	* @param {array} list - The list to be iterated over
 	*/
-
 	let map = (fn, list) => {
+
+		if ( !Array.isArray(list) ) { throw _error('Input must be an array'); }
+
+		let output = [];
+
+		forEach((value) => output.push(fn(value)), list)
+
+		return output;
 
 	};
 
@@ -47,8 +82,15 @@ var k = (() => {
 
 	};
 
+	/**
+	* Applies a reducer function to a list to
+	* @param {function} fn - The iterator function which receives the memo and current item from the list
+	* @param {*} memo - The initial value passed to the iterator function
+	* @param {array} list - The list to be iterated over
+	*/
+	let reduce = (fn, memo, list) => {
 
-	let reduce = (fn, memo, collection) => {
+
 
 	}
 
@@ -73,7 +115,10 @@ var k = (() => {
 
 	return {
 		curry,
-		path: curry(path)
+		path : curry(path),
+		forEach	: curry(forEach),
+		map : curry(map),
+
 	}
 
 })();
