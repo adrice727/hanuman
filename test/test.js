a0cbec39295b6e0985071fa065b6e5e0444b9b4b
Aaron Rice <adrice727@gmail.com>
2016-02-18 11:52:28 -0800

    Add test cases for filter.

--- test/test.js
+++ test/test.js
@@ -4,18 +4,19 @@ let assert = require('assert');
 let H = require('../dist/hanuman');
 
 // Data
-let numberList, fruitList, userList, user;
+let numbers, odds, fruit, users, user;
 
 // Functions
 let add;
 
 before(function(){
 
-  numberList = [1, 2, 3, 4, 5, 6];
+  numbers = [1, 2, 3, 4, 5, 6];
+  odds = [1, 3, 5, 7, 9, 11];
 
-  fruitList = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig'];
+  fruit = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig'];
 
-  userList = [
+  users = [
   	{id: '1ad3x', name: {first: 'Albert', last: 'King'}, age: 44},
   	{id: '4jde2', name: {first: 'Joe', last: 'Brown'}, age: 26},
   	{id: '7ssc1', name: {first: 'Susan', last: 'Wellington'}, age: 62}
@@ -36,6 +37,14 @@ describe('Hanuman#curry', function(){
   	let fn = H.curry(add)(10,11);
     assert.equal(fn(3), 24);
   });
+
+  it('should preserve context', () => {
+    let context = {c: 22};
+    let fn = H.curry(function(a,b) {return a + b + this.c});
+    assert.equal(fn.call(context, 1, 2), 25);
+  });
+
+  // it should preserve original function length
 });
 
 describe('Hanuman#forEach', () => {
@@ -43,14 +52,14 @@ describe('Hanuman#forEach', () => {
   it('should apply the supplied function to each item in an array', () => {
   	let list = [];
     let fn = (v,i) => { list.push({i, v}); }
-    H.forEach(fn, fruitList);
+    H.forEach(fn, fruit);
     assert(list[3].i === 3, list[4].v === 'date');
   });
 
-  it('should apply an iterate function to each property in an object', () => {
+  it('should apply the supplied function to each key-value pair in an object', () => {
     let obj = {};
   	let fn = (v,k) => { obj[k] = v;}
-    H.forEach(fn, userList[0]);
+    H.forEach(fn, users[0]);
     assert(obj.name.first === 'Albert', obj.name.last === 'King');
   });
 });
@@ -60,12 +69,28 @@ describe('Hanuman#map', () => {
   let fn = v => v * 10;
 
   it('should create a new list using the supplied function', () => {
-    let output = H.map(fn, numberList)
-    assert.equal(output[2], numberList[2] * 10);
+    let output = H.map(fn, numbers)
+    assert.equal(output[2], numbers[2] * 10);
   });
 
   it('should return an empty list if the input list is empty', () => {
     assert.equal(H.map(fn, []).length, 0);
+  });
+
+});
+
+describe('Hanuman#filter', () => {
+
+  let isEven = v => v % 2 === 0;
+
+  it('should create a new list containing values that satisfy the provided predicate', () => {
+    let output = H.filter(isEven, numbers)
+    assert(output.length === 3, output[2] === 6);
+  });
+
+  it('should return an empty list if none of the items in the list satisfy the predicate', () => {
+    let output = H.filter(isEven, odds);
+    assert.equal(output.length, 0);
   });
 
 });
